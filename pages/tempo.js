471e020cfcb472d939be7b82d27ad083569eeb04
hailtonnoc <76750840+hailtonnoc@users.noreply.github.com>
2021-01-22 19:17:44 -0300

    ok

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,14 +1,23 @@
-function Tempo()
+function Tempo(props)
 {
-const dynamicDate = new Date();
-const dynamicDateString = dynamicDate.toGMTString();
+    const dynamicDate = new Date();
+    const dynamicDateString = dynamicDate.toGMTString();
 
-return(
-    <div>
-        {dynamicDateString} (dinâmico)
-    </div>
-)
+    return (
+        <div>
+            <div>{dynamicDateString} (dinâmico)</div>
+            <div>{props.staticDateString} (estático)</div>
+        </div>
+    )
 
+}
+
+export function getStaticProps()
+{
+    const dynamicDate = new Date();
+    const dynamicDateString = dynamicDate.toGMTString();
+
+    return {props: {staticDateString}}
 }
 
 export default Tempo;
