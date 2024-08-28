f30ff1649b1dbcb3cc985f2434b72792998c7546
hailtonnoc <hailton.noc@gmail.com>
2024-08-28 10:27:11 -0300

    subindo landing

--- motor.js
+++ motor.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     const radios = document.querySelectorAll('input[name="widthOption"]');
     const iframe = document.querySelector('iframe#contabil');
+    const iframe2 = document.querySelector('iframe#landg');
 
     radios.forEach(radio => {
         radio.addEventListener('change', function() {
             iframe.width = this.value;
+            iframe2.width = this.value;
         });
     });
 });
